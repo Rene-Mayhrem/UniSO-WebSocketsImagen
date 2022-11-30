41dc7dd08585d8974d5c754c6df9b6b96076371e
Rene-Mayhrem <renecruz.0711@gmail.com>
2022-11-30 16:57:35 -0600

    update

--- tes-1/Image_socket.py
+++ tes-1/Image_socket.py
@@ -3,18 +3,58 @@ from tkinter import *
 from tkinter import filedialog
 import socket
 import cv2
+from PIL import Image, ImageOps
 
-c = 0
-q1 = input('IP address: ')
-q2 = int(input('Port: '))
-s = socket.socket()
-s.bind((q1, q2))
 
 data = filedialog.askopenfile(initialdir="/")
 path = str(data.name)
-image = open(path, 'rb')
+pic = cv2.imread(path)
+(B, G, R) = cv2.split(pic)
+
+
+def edge_img():
+    pic_1 = cv2.Canny(pic, 100, 200)
+    final_pic = Image.open(pic_1)
+    final_pic.save('pythonimage.jpg')
+
+def bgr2rgb_img():
+    pic_2 = cv2.cvtColor(pic, cv2.COLOR_BGR2RGB)
+    final_pic = Image.open(pic_2)
+    final_pic.save('pythonimage.jpg')
+
+def print_menu():
+    print ('2 -- Option 2: Margenes' )
+    print ('3 -- Option 3: BGR a RGB' )
+    print ('4 -- Exit' )
+
+def menu():
+    print_menu()
+    opc = int(input("Selecciona filtro: "))
+    while True:
+        if opc == 2:
+            edge_img()
+            break
+        if opc == 3:
+            bgr2rgb_img()
+            break
+        if opc == 4:
+            break
+
+menu()
+
+
+
+c = 0
+port = 5555
+q1 = input('IP address: ')
+q2 = port
+s = socket.socket()
+s.bind((q1,  port))
+
+image = open('pythonimage.jpg', 'rb')
 s.listen()
 c, address = s.accept()
 if c!= 0:
     for i in image:
         c.send(i)
+
--- tes-1/client.py
+++ tes-1/client.py
@@ -2,17 +2,19 @@ import socket
 import cv2
 import time
 
+port = 5555
+
 c = socket.socket()
 q = input('Enter IP address where you want o receive the image: ')
-p = int(input("Enter port number: "))
-r = input("Enter the extensionof the selected file (jpg - png - bmp)")
-s = "pythonimage."+r
+s = "pythonimage_client.jpg"
 print(s)
 condition = True
-c.connect((q, p))
+c.connect((q, port))
+
 f = open(s, 'wb')
 while condition:
     image = c.recv(1024)
     if str(image) == "b''":
         condition = False
     f.write(image)
+
